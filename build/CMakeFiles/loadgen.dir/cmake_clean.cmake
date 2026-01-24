1ed71d5995a9e153f9e6ca30c6e50da53c863be7
xc <xc@node0.xc-287049.dpmfs-pg0.wisc.cloudlab.us>
2026-01-24 08:37:48 -0600

    polish random

---
+++ build/CMakeFiles/loadgen.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/loadgen.dir/CPU/CPULoadGenerator.cpp.o"
+  "CMakeFiles/loadgen.dir/CPU/CPULoadGenerator.cpp.o.d"
+  "CMakeFiles/loadgen.dir/GPU/GPULoadGenerator.cu.o"
+  "CMakeFiles/loadgen.dir/GPU/GPULoadGenerator.cu.o.d"
+  "CMakeFiles/loadgen.dir/main.cpp.o"
+  "CMakeFiles/loadgen.dir/main.cpp.o.d"
+  "loadgen"
+  "loadgen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/loadgen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
